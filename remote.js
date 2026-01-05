9448b03ec093ba01c15f76dc5a61cc45f9d4698d
Lily <jnsmith458@gmail.com>
2026-01-05 14:57:16 -0500

    Implement main function for server hacking automation

---
+++ remote.js
@@ -0,0 +1,16 @@
+export async function main(ns) {
+  var host = ns.getHostname();
+  var sec = ns.getServerSecurityLevel(host)
+  var minSec = ns.getServerMinSecurityLevel(host)
+  var money = ns.getServerMoneyAvailable(host)
+  var maxMoney = ns.getServerMaxMoney(host)
+  while (true) {
+    while (sec > minSec + 1) {
+      await ns.weaken(host);
+    }
+    while (money > maxMoney * 0.9) {
+      await ns.grow(host);
+    }
+    await ns.hack(host);
+  }
+}
