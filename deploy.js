2051f89faad463016486590ed49747e78e778c5b
Lily <jnsmith458@gmail.com>
2026-01-05 15:35:05 -0500

    Refactor main function in deploy.js for new host

--- deploy.js
+++ deploy.js
@@ -1,5 +1,12 @@
-export async function main(ns) {
-  var host = "nectar-net"
-  ns.nuke(host);
-  ns.scp("remote.js", host);
-}
+  export async function main(ns) {
+  const host = "neo-net"
+  const serverMaxRam = ns.getServerMaxRam(host)
+  const serverThreads = serverMaxRam / 2.45
+  const serverThreadsRnd = Math.floor(serverThreads)
+  await ns.nuke(host);
+  await ns.brutessh(host);
+  //await ns.ftpcrack(hostName);
+  await ns.scp("remote.js", host, "home");
+  await ns.exec("remote.js", host, {threads: serverThreadsRnd});
+  await ns.alert("hostName:"+host+"\n"+"serverMaxRam:"+serverMaxRam+"\n"+"threads:"+serverThreadsRnd);
+  }
