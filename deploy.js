e5334ad02d99a2488fd71ba544d0f501d40b91ee
Lily <jnsmith458@gmail.com>
2026-01-05 16:19:58 -0500

    Change target host from 'neo-net' to 'phantasy'

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 export async function main(ns) {
-  const host = "neo-net"
+  const host = "phantasy"
   const serverMaxRam = ns.getServerMaxRam(host)
   const serverThreads = serverMaxRam / 2.45
   const serverThreadsRnd = Math.floor(serverThreads)
@@ -7,12 +7,12 @@ export async function main(ns) {
     await ns.brutessh(host);
   }
   if (ns.fileExists("FTPCrack.exe", "home")) {
-    await ns.ftpcrack(hostName);
+    await ns.ftpcrack(host);
   }
   if (ns.fileExists("NUKE.exe", "home")) {
     await ns.nuke(host);
   }
   await ns.scp("remote.js", host, "home");
   await ns.exec("remote.js", host, { threads: serverThreadsRnd });
-  await ns.alert("hostName:" + host + "\n" + "serverMaxRam:" + serverMaxRam + "\n" + "threads:" + serverThreadsRnd);
+  await ns.alert("host:" + host + "\n" + "serverMaxRam:" + serverMaxRam + "\n" + "threads:" + serverThreadsRnd);
 }
