ab9ffcdeb5568e91b8efe3dde2e233a2448a67a5
Lily <jnsmith458@gmail.com>
2026-01-05 16:50:51 -0500

    Refactor deploy.js for new host and skill checks

--- deploy.js
+++ deploy.js
@@ -1,18 +1,23 @@
 export async function main(ns) {
-  const host = "phantasy"
-  const serverMaxRam = ns.getServerMaxRam(host)
-  const serverThreads = serverMaxRam / 2.45
-  const serverThreadsRnd = Math.floor(serverThreads)
-  if (ns.fileExists("BruteSSH.exe", "home")) {
-    await ns.brutessh(host);
+  var host = "omega-net"
+  var serverMaxRam = ns.getServerMaxRam(host)
+  var serverThreads = serverMaxRam / 2.45
+  var serverThreadsRnd = Math.floor(serverThreads)
+  var hackSkill = ns.getHackingLevel(host)
+  var reqHackSkill = ns.getServerRequiredHackingLevel(host)
+  if (hackSkill >= reqHackSkill) {
+    if (ns.fileExists("BruteSSH.exe", "home")) {
+      await ns.brutessh(host);
+    }
+    if (ns.fileExists("FTPCrack.exe", "home")) {
+      await ns.ftpcrack(host);
+    }
+    if (ns.fileExists("NUKE.exe", "home")) {
+      await ns.nuke(host);
+    }
+    await ns.scp("remote.js", host, "home");
+    await ns.exec("remote.js", host, { threads: serverThreadsRnd });
+    await ns.alert("host:" + host + "\n" + "serverMaxRam:" + serverMaxRam + "\n" + "threads:" + serverThreadsRnd);
   }
-  if (ns.fileExists("FTPCrack.exe", "home")) {
-    await ns.ftpcrack(host);
-  }
-  if (ns.fileExists("NUKE.exe", "home")) {
-    await ns.nuke(host);
-  }
-  await ns.scp("remote.js", host, "home");
-  await ns.exec("remote.js", host, { threads: serverThreadsRnd });
-  await ns.alert("host:" + host + "\n" + "serverMaxRam:" + serverMaxRam + "\n" + "threads:" + serverThreadsRnd);
+  else { ns.alert("Insufficient hacking skill!") }
 }
