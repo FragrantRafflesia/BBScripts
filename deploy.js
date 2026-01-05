764239dde3fab14b3b39a8d4971def84a73fb3a5
Lily <jnsmith458@gmail.com>
2026-01-05 15:44:14 -0500

    Refactor deploy.js to check for hacking tools

--- deploy.js
+++ deploy.js
@@ -1,12 +1,18 @@
-  export async function main(ns) {
+export async function main(ns) {
   const host = "neo-net"
   const serverMaxRam = ns.getServerMaxRam(host)
   const serverThreads = serverMaxRam / 2.45
   const serverThreadsRnd = Math.floor(serverThreads)
-  await ns.nuke(host);
-  await ns.brutessh(host);
-  //await ns.ftpcrack(hostName);
-  await ns.scp("remote.js", host, "home");
-  await ns.exec("remote.js", host, {threads: serverThreadsRnd});
-  await ns.alert("hostName:"+host+"\n"+"serverMaxRam:"+serverMaxRam+"\n"+"threads:"+serverThreadsRnd);
+  if (ns.fileExists("BruteSSH.exe", "home")) {
+    await ns.brutessh(host);
   }
+  if (ns.fileExists("FTPCrack.exe", "home")) {
+    await ns.ftpcrack(hostName);
+  }
+  if (ns.fileExists("NUKE.exe", "home")) {
+    await ns.nuke(host);
+  }
+  await ns.scp("remote.js", host, "home");
+  await ns.exec("remote.js", host, { threads: serverThreadsRnd });
+  await ns.alert("hostName:" + host + "\n" + "serverMaxRam:" + serverMaxRam + "\n" + "threads:" + serverThreadsRnd);
+}
