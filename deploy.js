77b373c5036666771c26654d584d98c4ce13853b
Lily <jnsmith458@gmail.com>
2026-01-05 19:32:58 -0500

    Change target host to 'CSEC' and update scripts

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 export async function main(ns) {
-  var host = "omega-net"
+  var host = "CSEC"
   var serverMaxRam = ns.getServerMaxRam(host)
   var serverThreads = serverMaxRam / 2.45
   var serverThreadsRnd = Math.floor(serverThreads)
@@ -12,10 +12,17 @@ export async function main(ns) {
     if (ns.fileExists("FTPCrack.exe", "home")) {
       await ns.ftpcrack(host);
     }
-    if (ns.fileExists("NUKE.exe", "home")) {
-      await ns.nuke(host);
+    if (ns.fileExists("relaySMTP.exe", "home")) {
+      await ns.relaysmtp(host);
     }
-    await ns.scp("remote.js", host, "home");
+    if (ns.fileExists("HTTPWorm.exe", "home")) {
+      await ns.httpworm(host);
+    }
+    if (ns.fileExists("remote.js", host)) {
+      ns.rm("remote.js", host)
+      await ns.scp("remote.js", host, "home");
+    }
+    else { await ns.scp("remote.js", host, "home"); }
     await ns.exec("remote.js", host, { threads: serverThreadsRnd });
     await ns.alert("host:" + host + "\n" + "serverMaxRam:" + serverMaxRam + "\n" + "threads:" + serverThreadsRnd);
   }
