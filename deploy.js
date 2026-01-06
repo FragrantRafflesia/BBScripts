16014ca75cb5c17af4a7414847ac287b423eb12c
Lily <jnsmith458@gmail.com>
2026-01-05 23:34:11 -0500

    Add 0 ram warning

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 export async function main(ns) {
-  var host = "omega-net"
+  var host = "crush-fitness"
   var serverMaxRam = ns.getServerMaxRam(host)
   var serverThreads = serverMaxRam / 2.45
   var serverThreadsRnd = Math.floor(serverThreads)
@@ -18,17 +18,22 @@ export async function main(ns) {
     if (ns.fileExists("HTTPWorm.exe", "home")) {
       await ns.httpworm(host);
     }
-        if (ns.fileExists("NUKE.exe", "home")) {
+    if (ns.fileExists("NUKE.exe", "home")) {
       await ns.nuke(host);
     }
-    if (ns.fileExists("remote.js", host)) {
-      ns.killall(host)
-      ns.rm("remote.js", host)
-      await ns.scp("remote.js", host, "home");
+    if (serverMaxRam > 0) {
+      if (ns.fileExists("remote.js", host)) {
+        ns.killall(host)
+        ns.rm("remote.js", host)
+        await ns.scp("remote.js", host, "home");
+      }
+      else { await ns.scp("remote.js", host, "home"); }
+
+      await ns.exec("remote.js", host, { threads: serverThreadsRnd });
+      await ns.alert("host:" + host + "\n" + "serverMaxRam:" + serverMaxRam + "\n" + "threads:" + serverThreadsRnd);
     }
-    else { await ns.scp("remote.js", host, "home"); }
-    await ns.exec("remote.js", host, { threads: serverThreadsRnd });
-    await ns.alert("host:" + host + "\n" + "serverMaxRam:" + serverMaxRam + "\n" + "threads:" + serverThreadsRnd);
+    else { ns.alert("No RAM!"); }
   }
+
   else { ns.alert("Insufficient hacking skill!") }
 }
