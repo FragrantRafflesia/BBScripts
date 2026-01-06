b15d710fbd0465e783a8b7c308c53fa397346429
Lily <jnsmith458@gmail.com>
2026-01-06 02:24:43 -0500

    Refactor main function to use ns.peek for host

--- remote.js
+++ remote.js
@@ -1,10 +1,10 @@
 export async function main(ns) {
-  let host = ns.getHostname();
-  let minSec = ns.getServerMinSecurityLevel(host);
-  let maxMoney = ns.getServerMaxMoney(host);
   while (true) {
-    let sec = ns.getServerSecurityLevel(host);
-    let money = ns.getServerMoneyAvailable(host);
+    var host = ns.peek(1)
+    var minSec = ns.getServerMinSecurityLevel(host);
+    var maxMoney = ns.getServerMaxMoney(host);
+    var sec = ns.getServerSecurityLevel(host);
+    var money = ns.getServerMoneyAvailable(host);
     if (sec > minSec + 1) {
       await ns.weaken(host);
     } else if (money < maxMoney * 0.9) {
