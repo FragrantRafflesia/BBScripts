acaea9d97b9864dc006cf0437df91ce1657b3bd1
Lily <jnsmith458@gmail.com>
2026-01-07 00:58:23 -0500

    Refactor deploy.js to manage multiple hosts

--- deploy.js
+++ deploy.js
@@ -1,42 +1,24 @@
+/** @param {NS} ns */
 export async function main(ns) {
-  var host = "crush-fitness"
-  var serverMaxRam = ns.getServerMaxRam(host)
-  var serverThreads = serverMaxRam / 2.45
-  var serverThreadsRnd = Math.floor(serverThreads)
-  var hackSkill = ns.getHackingLevel(host)
-  var reqHackSkill = ns.getServerRequiredHackingLevel(host)
-  if (hackSkill >= reqHackSkill) {
-    if (ns.fileExists("BruteSSH.exe", "home")) {
-      await ns.brutessh(host);
-    }
-    if (ns.fileExists("FTPCrack.exe", "home")) {
-      await ns.ftpcrack(host);
-    }
-    if (ns.fileExists("relaySMTP.exe", "home")) {
-      await ns.relaysmtp(host);
-    }
-    if (ns.fileExists("HTTPWorm.exe", "home")) {
-      await ns.httpworm(host);
-    }
-    if (ns.fileExists("SQLInject.exe", "home")) {
-      await ns.sqlinject(host);
-    }
-    if (ns.fileExists("NUKE.exe", "home")) {
-      await ns.nuke(host);
-    }
-    if (serverMaxRam > 0) {
-      if (ns.fileExists("remote.js", host)) {
-        ns.killall(host)
-        ns.rm("remote.js", host)
-        await ns.scp("remote.js", host, "home");
+  var hosts = ["4sigma", "aerocorp", "aevum-police", "alpha-ent", "applied-energetics", "avmnite-02h", "b-and-a", "blade", "catalyst", "clarkinc", "computek", "crush-fitness", "CSEC", "darkweb", "defcomm", "deltaone", "ecorp", "foodnstuff", "fulcrumassets", "fulcrumtech", "galactic-cyber", "global-pharm", "harakiri-sushi", "helios", "hong-fang-tea", "I.I.I.I", "icarus", "infocomm", "iron-gym", "joesguns", "johnson-ortho", "kuai-gong", "lexo-corp", "max-hardware", "megacorp", "microdyne", "millenium-fitness", "n00dles", "nectar-net", "neo-net", "netlink", "nova-med", "nwo", "omega-net", "omnia", "omnitek", "phantasy", "powerhouse-fitness", "rho-construction", "rothman-uni", "run4theh111z", "sigma-cosmetics", "silver-helix", "snap-fitness", "solaris", "stormtech", "summit-uni", "syscore", "taiyang-digital", "The-Cave", "the-hub", "titan-labs", "unitalife", "univ-energy", "vitalife", "zb-def", "zb-institute", "zer0", "zeus-med"]
+  var index = 0
+  while (true) {
+    var target = hosts[index]
+    var cRam = ns.getServerMaxRam(target);
+    var threads = Math.floor(cRam / 4);
+    var pwned = ns.getServer(target).hasAdminRights
+    if (cRam > 4) {
+      if (pwned) {
+        if (!ns.fileExists("share.js", target)) { ns.scp("share.js", target, "home") }
+        if (!ns.scriptRunning("share.js", target)) { ns.exec("share.js", target, threads) }
+        if (ns.getServerUsedRam(target) <= (cRam * 0.9)) {
+          ns.killall(target);
+          ns.exec("share.js", target, threads)
+        }
       }
-      else { await ns.scp("remote.js", host, "home"); }
-
-      await ns.exec("remote.js", host, { threads: serverThreadsRnd });
-      await ns.alert("host:" + host + "\n" + "serverMaxRam:" + serverMaxRam + "\n" + "threads:" + serverThreadsRnd);
     }
-    else { ns.alert("No RAM!"); }
+    index++;
+    if (index >= 69) { index = 0 };
+    await ns.sleep(3000);
   }
-
-  else { ns.alert("Insufficient hacking skill!") }
 }
