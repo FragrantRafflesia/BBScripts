5c698ef132ac8a2842b7a5708e6ff7a724d29c0a
Lily <jnsmith458@gmail.com>
2026-01-07 00:56:54 -0500

    Refactor main function for better security checks

--- remote.js
+++ remote.js
@@ -1,16 +1,21 @@
 export async function main(ns) {
+  var name = ns.getHostname()
+  var port = name.replace("srv", "");
   while (true) {
-    var host = ns.peek(1)
+    if (name == "home") { port = 26 }
+    var host = ns.peek(port)
     var minSec = ns.getServerMinSecurityLevel(host);
     var maxMoney = ns.getServerMaxMoney(host);
     var sec = ns.getServerSecurityLevel(host);
     var money = ns.getServerMoneyAvailable(host);
-    if (sec > minSec + 1) {
-      await ns.weaken(host);
-    } else if (money < maxMoney * 0.9) {
-      await ns.grow(host);
-    } else {
-      await ns.hack(host);
+    if (ns.hasRootAccess(host)) {
+      if (sec > minSec + 1) {
+        await ns.weaken(host);
+      } else if (money < maxMoney * 0.9) {
+        await ns.grow(host);
+      } else {
+        await ns.hack(host);
+      }
     }
   }
 }
